c020118f83b8ef223182dded1da2336dc777eab7
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2024-10-11 08:32:42 -0500

    test: adds custom render for component tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,7 @@
 import "@testing-library/jest-dom";
+
+import { setConfig } from "next/config";
+import config from "./next.config.mjs";
+setConfig({
+  publicRuntimeConfig: config.publicRuntimeConfig,
+});
---
+++ test-utils.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement } from "react";
+import { render as _render, RenderOptions } from "@testing-library/react";
+import { Provider as GlobusAuthorizationManagerProvider } from "@globus/react-auth-context";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <GlobusAuthorizationManagerProvider client="abc" redirect="#">
+      {children}
+    </GlobusAuthorizationManagerProvider>
+  );
+};
+
+// eslint-disable-next-line import/export
+export * from "@testing-library/react";
+// eslint-disable-next-line import/export
+export const render = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, "wrapper">,
+) => {
+  _render(ui, { wrapper: Providers, ...options });
+};
