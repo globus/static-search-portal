8fac9a178c49b01aef5ff83e0dc384a9d6fccfac
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2024-11-04 19:04:06 -0600

    fix: Ensure application path is accounted for in OAuth redirects

--- src/hooks/useSignIn.ts
+++ src/hooks/useSignIn.ts
@@ -1,13 +1,14 @@
 import { useOAuthStore } from "@/store/oauth";
 import { useGlobusAuth } from "@globus/react-auth-context";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export function useSignIn() {
   const auth = useGlobusAuth();
   const oauthStore = useOAuthStore();
+  const pathname = usePathname();
+  const search = useSearchParams();
   return async () => {
-    oauthStore.setReplaceWith(
-      window.location.pathname + window.location.search,
-    );
+    oauthStore.setReplaceWith(`${pathname}${search ? `?${search}` : ""}`);
     await auth.authorization?.login();
   };
 }
