8b768c772fb7cc1071b8c39acc2d75ea99e3ed31
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2026-01-07 11:54:05 -0600

    feat: Upgrade to Next.js 16

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,1 @@
 import "@testing-library/jest-dom";
-
-import { setConfig } from "next/config";
-import config from "./next.config.mjs";
-setConfig({
-  publicRuntimeConfig: config.publicRuntimeConfig,
-});
